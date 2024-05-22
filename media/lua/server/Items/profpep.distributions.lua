5fafacce810ec28b9ad619950bf15ab62f7047aa
Moon-794 <moon0795@outlook.com>
2024-05-22 04:43:51 +0100

    Added peppy to vending machine loot table

---
+++ media/lua/server/Items/profpep.distributions.lua
@@ -0,0 +1,4 @@
+require 'Items/ProceduralDistributions'
+
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, "ProfPep.ProfessorPeppy");
+table.insert(SuburbsDistributions["all"]["vendingpop"].items, 100);
